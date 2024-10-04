5a00ab487ad8da5a961a2ccc65a80f3c084816ba
dantvi <daniel.tving@gmail.com>
2024-10-04 16:22:21 +0200

    updated mobile share functionality

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ const changeBubbleVisibility = () => {
     // Show bubble
     shareBubble.classList.remove('hidden');
     shareBtn.style.backgroundColor = '#6e8098';
+    shareBtn.style.marginTop = '12px';
     shareIcon.style.color = '#ffffff'
     isHidden = false;
   } else {
     // Hide bubble
     shareBubble.classList.add('hidden');
     shareBtn.style.backgroundColor = '#ecf2f8';
+    shareBtn.style.marginTop = '0';
     shareIcon.style.color = '#6e8098'
     isHidden = true;
   }
